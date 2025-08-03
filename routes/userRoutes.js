b7ccdf830e6d2d8c5350e1dbb4846c941f9f6517
tsheringpema <tpems2019@gmail.com>
2025-08-03 20:02:15 +0600

    project1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/UserController');
 const jwt = require('jsonwebtoken');
 
 // Middleware to check if user is logged in
